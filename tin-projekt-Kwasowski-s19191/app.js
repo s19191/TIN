64ba16eaacd8ea2fb9507f6b23b413fb47f56350
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-01-12 23:22:12 +0100

    dodanie początku logowania

--- tin-projekt-Kwasowski-s19191/app.js
+++ tin-projekt-Kwasowski-s19191/app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 const ksiazkaRouter = require('./routes/ksiazkaRoute');
 const magazynRouter = require('./routes/magazynRoute');
 const stanWMagazynieRouter = require('./routes/stanWMagazynieRoute');
@@ -12,12 +12,9 @@ const ksiazkaApiRouter = require('./routes/api/KsiazkaApiRoute');
 const magazynApiRouter = require('./routes/api/MagazynApiRoute');
 const stanWMagazynieApiRouter = require('./routes/api/StanWMagazynieApiRoute');
 const sequelizeInit = require('./config/sequelize/init');
-sequelizeInit()
-    .catch(err => {
-      console.log(err);
-    });
+const session = require('express-session');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +26,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+    secret: 'my_secret_password',
+    resave: false
+}));
+
 app.use('/', indexRouter);
 app.use('/ksiazka', ksiazkaRouter);
 app.use('/magazyn', magazynRouter);
@@ -37,6 +39,10 @@ app.use('/api/ksiazki', ksiazkaApiRouter);
 app.use('/api/magazyny', magazynApiRouter);
 app.use('/api/stanWMagazynie', stanWMagazynieApiRouter);
 
+app.use(session({
+    secret: 'my_secret_password',
+    resave: false
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,5 +59,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+sequelizeInit()
+    .catch(err => {
+        console.log(err);
+    });
 
 module.exports = app;
