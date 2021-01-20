09d575c055f80b54d0c9c41675ef2e018ace2fee
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-01-20 02:09:12 +0100

    do "Stworzenie ścieżki nawigacyjnej i przekazanie parametru" chyba pokazanie szczegółów mogło by za chwilę działać

--- tin-react-projekt-s19191/src/App.js
+++ tin-react-projekt-s19191/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/fragments/Navigation';
 import MainContent from './components/other/MainContent';
 import Footer from './components/fragments/Footer';
 import BookList from './components/book/BookList';
+import BookDetails from './components/book/BookDetails';
 import {
     BrowserRouter as Router,
     Switch,
@@ -12,17 +13,18 @@ import {
 
 function App() {
   return (
-      <div>
-        <Header />
-        <Navigation />
-          <Router>
-          <Switch>
-              <Route exact path="/" component={MainContent} />
-              <Route exact path="/books" component={BookList} />
-          </Switch>
-          </Router>
-        <Footer />
-      </div>
+      <Router>
+          <div>
+              <Header />
+              <Navigation />
+              <Switch>
+                  <Route exact path="/" component={MainContent} />
+                  <Route exact path="/books" component={BookList} />
+                  <Route exact path="/books/details/:Id_Ksiazka" component={BookDetails} />
+              </Switch>
+              <Footer />
+          </div>
+      </Router>
   );
 }
 
--- tin-react-projekt-s19191/src/components/fragments/Navigation.js
+++ tin-react-projekt-s19191/src/components/fragments/Navigation.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Link
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
     return (
         <nav>
             <ul>
-                <Router>
                 <li><Link to="/">Strona główna</Link></li>
                 <li><Link to="/books">Książki</Link></li>
                 <li><Link to="/magazyn">Magazyny</Link></li>
                 <li><Link to="/stanWMagazynie">Stan w magazynach</Link></li>
-                </Router>
             </ul>
         </nav>
     )
