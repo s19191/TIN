5b68ae9072f7a99e6d3117710f2e9f7c8bde16fb
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-01-28 11:58:55 +0100

    działa pobieranie detalów stanu w magazynie

--- tin-react-projekt-s19191/src/components/conditionInWarehouse/ConditionInWarehouseDetails.js
+++ tin-react-projekt-s19191/src/components/conditionInWarehouse/ConditionInWarehouseDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, useParams} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { getConditionInwarehouseByIdApiCall } from '../../apiCalls/conditionInwarehouseApiCalls';
 import ConditionInWarehouseDetailsData from "../conditionInWarehouse/ConditionInWarehouseDetailsData";
 
